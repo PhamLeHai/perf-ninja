91b9b49cd61f23f7831c2eae84e57d5dca5b31c2
Pham Le Hai <phamlehaicnttk56@gmail.com>
2024-03-13 20:43:35 +0800

    [lookup_tables_1] use hardcoded array instead of if-else

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,5 +1,19 @@
 #include "solution.hpp"
 
+#include <algorithm>
+#include <cstdint>
+
+bool ready = false;
+std::array<uint8_t, 100> vals;
+
+#ifdef SOLUTION
+static std::size_t mapToBucket(std::size_t v) {    // diff
+  if(v < 0 || v >= 100) {
+    return -1;
+  }
+  return vals[v];
+}
+#else
 static std::size_t mapToBucket(std::size_t v) {    // diff
   if (v >= 0 && v < 13)        return 0; // 13
   else if (v >= 13 && v < 29)  return 1; // 16
@@ -10,9 +24,23 @@ static std::size_t mapToBucket(std::size_t v) {    // diff
   else if (v >= 83 && v < 100) return 6; // 17
   return -1;  // let it crash
 }
+#endif
+
+void init() {
+  std::fill(vals.begin(), vals.begin() + 13, 0);
+  std::fill(vals.begin() + 13, vals.begin() + 29, 1);
+  std::fill(vals.begin() + 29, vals.begin() + 41, 2);
+  std::fill(vals.begin() + 41, vals.begin() + 53, 3);
+  std::fill(vals.begin() + 53, vals.begin() + 71, 4);
+  std::fill(vals.begin() + 71, vals.begin() + 83, 5);
+  std::fill(vals.begin() + 83, vals.begin() + 100, 6);
+}
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
   std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+  if(!ready) {
+    init();
+  }
   for (auto v : values) {
     retBuckets[mapToBucket(v)]++;
   }
