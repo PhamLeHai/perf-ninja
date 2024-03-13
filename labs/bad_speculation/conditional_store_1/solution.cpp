34c7127d015fad5166db6ef154ecf7cf60b1992b
Pham Le Hai <phamlehaicnttk56@gmail.com>
2024-03-13 20:36:59 +0800

    [conditional_store_1] always store and conditionally increase

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,6 +1,19 @@
 
 #include "solution.h"
 
+#ifdef SOLUTION
+// Select items which have S.first in range [lower..upper]
+std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
+                   const std::uint32_t lower, const std::uint32_t upper) {
+  std::size_t count = 0;
+  for (const auto item : input) {
+    output[count] = item;
+    count += (lower <= item.first) && (item.first <= upper);
+  }
+  return count;
+}
+
+#else
 // Select items which have S.first in range [lower..upper]
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
@@ -12,3 +25,4 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
   }
   return count;
 }
+#endif
